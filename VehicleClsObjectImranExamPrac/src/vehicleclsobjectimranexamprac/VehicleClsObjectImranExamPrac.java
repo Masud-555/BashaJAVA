6208f3adb29c76c8069968f1894c604e8510a51f
Abdullah Al Masud <abdullahalmasud011@gmail.com>
2025-04-29 19:01:57 +0600

    add

---
+++ VehicleClsObjectImranExamPrac/src/vehicleclsobjectimranexamprac/VehicleClsObjectImranExamPrac.java
@@ -0,0 +1,19 @@
+
+package vehicleclsobjectimranexamprac;
+
+
+public class VehicleClsObjectImranExamPrac {
+
+    
+    public static void main(String[] args) {
+        
+        Truck t = new Truck();
+        
+        t.setRegularPrice(5000000);
+        t.setWeight(3000);
+        System.out.println(t.getSalePrice());
+                
+        
+    }
+    
+}

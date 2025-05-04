49757a4dbed29b14f533a842ce4e44fc1b855b01
Abdullah Al Masud <abdullahalmasud011@gmail.com>
2025-05-04 16:55:03 +0600

    add

---
+++ FileClsTaskForMidExm/src/fileclstaskformidexm/FileClsTaskForMidExm.java
@@ -0,0 +1,57 @@
+
+package fileclstaskformidexm;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+public class FileClsTaskForMidExm {
+
+    
+    public static void main(String[] args) {
+        
+        
+        File f = new File("C:\\Users\\Admin\\Desktop\\Mas.txt");
+        
+                    
+        try {
+            
+            PrintWriter pw = new PrintWriter(f);
+            
+             pw.write("I Love Bangladesh \n");
+            pw.println("My Name Is Masud ");
+            pw.append("I am very good person.");
+            
+            pw.close();
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FileClsTaskForMidExm.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+            
+             
+            try {
+                
+                
+               Scanner s = new Scanner(f);
+               
+                 while (s.hasNext()) {
+                System.out.println(s.nextLine());
+
+            }
+               
+               
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(FileClsTaskForMidExm.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+                       
+        } 
+        
+    } 
+        
+       
